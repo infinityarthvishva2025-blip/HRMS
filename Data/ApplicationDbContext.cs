d207a7b2ce67c383e4601e2e5024cbbcfdc13e5f
IA <infinityarthvishva2025@gmail.com>
2025-12-17 16:09:25 +0530

    BM

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,68 +1,4 @@
-﻿//using HRMS.Models;
-//using Microsoft.EntityFrameworkCore;
-
-//namespace HRMS.Data
-//{
-//    public class ApplicationDbContext : DbContext
-//    {
-//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-//            : base(options)
-//        {
-//        }
-
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//                optionsBuilder.UseSqlServer(
-//                    "",
-//                    sql => sql.EnableRetryOnFailure(
-//                        maxRetryCount: 5,
-//                        maxRetryDelay: TimeSpan.FromSeconds(10),
-//                        errorNumbersToAdd: null
-//                    )
-//                );
-//            }
-//        }
-
-//        // ===================
-//        // Actual DB Tables
-//        // ===================
-//        public DbSet<Attendance> Attendances { get; set; }
-//        public DbSet<Employee> Employees { get; set; }
-//        public DbSet<Position> Positions { get; set; }
-//        public DbSet<Payroll> Payrolls { get; set; }
-//        public DbSet<Leave> Leaves { get; set; }
-//        public DbSet<GurukulVideo> GurukulVideos { get; set; }
-//        public DbSet<GurukulProgress> GurukulProgress { get; set; }
-//        public DbSet<Expenses> Expenses { get; set; }
-//        public DbSet<Announcement> Announcements { get; set; }
-//        public DbSet<LeaveApprovalRoute> LeaveApprovalRoutes { get; set; }
-//        public DbSet<Hr> Hrs { get; set; }
-//        public DbSet<GeoTag> GeoTags => Set<GeoTag>();
-
-//        protected override void OnModelCreating(ModelBuilder modelBuilder)
-//        {
-//            base.OnModelCreating(modelBuilder);
-
-//            // Correct Attendance table mapping
-//            modelBuilder.Entity<Attendance>(entity =>
-//            {
-//                entity.ToTable("Attendances");
-//            });
-
-//            // Leave table mapping
-//            modelBuilder.Entity<Leave>()
-//                .HasOne(l => l.Employee)
-//                .WithMany()
-//                .HasForeignKey(l => l.EmployeeId)
-//                .OnDelete(DeleteBehavior.Restrict);
-
-//            modelBuilder.Entity<Leave>().ToTable("Leaves");
-//        }
-//    }
-//}
-
+﻿
 
 using HRMS.Models;
 using Microsoft.EntityFrameworkCore;
@@ -96,7 +32,7 @@ namespace HRMS.Data
 
         // DbSets
         public DbSet<Attendance> Attendances { get; set; }
-
+       
         public DbSet<Attendance> AttendanceRecords { get; set; }
         public DbSet<Payroll> Payrolls { get; set; }
         public DbSet<Employee> Employees { get; set; }
